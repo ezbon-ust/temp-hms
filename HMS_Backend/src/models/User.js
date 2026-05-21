3725373184e9d5ad7f52d73478b942c5b84dd64f
Ezbon Puthanangady Joy(UST <308327@ust.com>
2026-05-21 17:06:44 +0530

    backend v5

--- HMS_Backend/src/models/User.js
+++ HMS_Backend/src/models/User.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
     {   
         email: {type: String,required: true,unique: true,lowercase: true,trim: true,},
-        passwordHash: {type:String,required:true},
+        passwordHash: {type:String},
         status: {type:String,enum:['ACTIVE','PENDING','INACTIVE']},
         role : {type:String,enum:['OWNER','ADMIN','DOCTOR','RECEPTIONIST','CASHIER','NURSE' ,'LAB_TECH ','PHARMACIST']},
         employeeId: {type:String},
